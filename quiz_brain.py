6ad7911307ce459eb9d21f51d657d9360651dcda
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-04 09:42:22 +0200

    current score method and num questions attribute added

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,10 +3,23 @@ class Quizbrain:
 
     def __init__(self, question_list):
         self.question_number = 0
+        self.number_of_questions = 0
+        self.score = 0
         self.question_list = question_list
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        print(f"The current question number is: {self.question_number}")
-        input(f"{current_question.text} (True/False): ")
+        self.question_number += 1
+        answer = input(f"Q{self.question_number}: {current_question.text} (True/False): ")
+    
+    def current_score(self, answer):
+        #if answer == current.question.answer
+        if answer == current_question.ander:
+            self.score += 1
+            print(f"You are correct. Your score is: {self.score}")
+        else:
+            print(f"You are correct. Your score is: {self.score}")
+
+
+    
 
